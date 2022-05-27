b0b4bae1b9ad15f9dd12ff655f58c4811fac1285
a3drian <38135936+a3drian@users.noreply.github.com>
2022-05-27 17:31:04 +0300

    Updated version of installed "net-worth-shared". * Loading spinner is now used as a grid item. * Added ".gitignore" from Firebase in root directory. * Added simple logo for use in Firebase console. * Added medical category.

--- client/src/app/shared/constants/Categories.ts
+++ client/src/app/shared/constants/Categories.ts
@@ -4,6 +4,7 @@ export enum CATEGORY {
 	GOINGOUT = 'Going out',
 	GROCERIES = 'Groceries',
 	HOUSEHOLD = 'Household',
+	MEDICAL = 'Medical',
 	SERVICES = 'Services',
 	SHOPPING = 'Shopping',
 	SUBS = 'Subscription',
