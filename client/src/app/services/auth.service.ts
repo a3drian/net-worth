c1aef4f2f07d550dde234ce6152be1a672223c3c
a3drian <38135936+a3drian@users.noreply.github.com>
2022-05-27 17:29:18 +0300

    Added authentication service with Firebase social login integration. * Updated "InformationService" to take e-mail information from social profile. * Added "AuthGuard" to "app.module". * Added "IUser" interface and "GoogleUser" model.

---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import {
+	Auth,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+	UserCredential
+} from '@angular/fire/auth';
+// Models:
+import { GoogleUser, IUser } from 'src/app/models/User';
+// rxjs:
+import { BehaviorSubject, map, Observable, of } from 'rxjs';
+// Shared:
+import { Constants } from '../shared/Constants';
+import { log } from '../shared/Logger';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AuthService {
+
+	isInDebugMode: boolean = Constants.IN_DEBUG_MODE;
+
+	user = new BehaviorSubject<IUser | null>(null);
+
+	private USER_KEY = 'SPENDIT_USER';
+	private TOKEN_KEY = 'SPENDIT_TOKEN';
+
+	constructor(private auth: Auth) { }
+
+	async loginWithGoogle() {
+
+		let user: GoogleUser;
+		const provider = new GoogleAuthProvider();
+
+		await signInWithPopup(this.auth, provider)
+			.then(
+				(uc: UserCredential) => {
+					user = { ...uc.user } as IUser;
+					log('auth.service', this.loginWithGoogle.name, 'user:', user.displayName);
+
+					if (!user.accessToken) { return of(null); }
+
+					this.user.next(user);
+					const token = user.accessToken;
+					this.setSessionInfo(user, token);
+					return of(token);
+				}
+			)
+			.catch(e => {
+				log('auth.service', this.loginWithGoogle.name, 'Exception while signing in with Google:', e);
+				this.removeUserData();
+			});
+	}
+
+	async logout(): Promise<void> {
+		await signOut(this.auth)
+			.catch(e => log('auth.service', this.logout.name, 'Exception while signing out:', e))
+			.finally(() => this.removeUserData());
+	}
+
+	// Helper methods:
+	autoLogin(): void {
+		log('auth.service', this.autoLogin.name, '');
+
+		const session = this.getSessionInfo();
+		if (!session) { return; }
+
+		session.subscribe((user: IUser | null) => this.user.next(user));
+	}
+
+	// autoLogout(): void {}
+
+	isAuthenticated(): Observable<boolean> {
+		return this.getSessionInfo().pipe(map(u => u !== null));
+	}
+
+	private removeUserData() {
+		this.user.next(null);
+		sessionStorage.removeItem(this.USER_KEY);
+		sessionStorage.removeItem(this.TOKEN_KEY);
+	}
+
+	private setSessionInfo(user: IUser, token: string): void {
+		sessionStorage.setItem(this.USER_KEY, JSON.stringify(user));
+		sessionStorage.setItem(this.TOKEN_KEY, token);
+	}
+
+	private getSessionInfo(): Observable<IUser | null> {
+		const userSessionInfo = sessionStorage.getItem(this.USER_KEY);
+
+		if (!userSessionInfo) { return of(null); }
+
+		const sessionInfo = JSON.parse(userSessionInfo);
+		log('auth.service', this.getSessionInfo.name, 'Session info:', sessionInfo.email);
+		return of(sessionInfo);
+	}
+}
