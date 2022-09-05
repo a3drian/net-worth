63b255541328fa8248cad44279566bc67ece3607
a3drian <38135936+a3drian@users.noreply.github.com>
2022-09-05 18:06:51 +0300

    Added "policy" component because of requirement for OAuth verification.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,5 +7,6 @@ export interface AppRoute {
 
 export const appRoutes = Object.freeze({
 	login: <AppRoute>{ path: Constants.appEndpoints.LOGIN_URL },
-	dashboard: <AppRoute>{ path: Constants.appEndpoints.DASHBOARD_URL }
+	dashboard: <AppRoute>{ path: Constants.appEndpoints.DASHBOARD_URL },
+	policy: <AppRoute>{ path: Constants.appEndpoints.POLICY_URL }
 });
