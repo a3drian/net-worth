08278a7a76740ddcd488727c3a80a8a6372074e4
a3drian <38135936+a3drian@users.noreply.github.com>
2024-01-09 10:00:22 +0200

    Added new script for client.

--- api/src/shared/Logger.ts
+++ api/src/shared/Logger.ts
@@ -10,43 +10,29 @@ const log = function (
 	message: string,
 	object?: any
 ): void {
-	if (Constants.IN_DEBUG_MODE) {
-		if (!(caller.includes('(') || caller.includes(')'))) {
-			caller = caller + '()';
-		}
-		if (caller.includes('^')) {
-			caller = caller.replace('^', '');
-			caller = caller + '^';
-		}
-		if (object) {
-			const type = typeof (object);
-			// console.log('type:', type);
-			if (type === 'boolean') {
-				console.group(
-					Color.BgWhite + Color.FgBlack, className, Color.Reset,
-					Color.BgBlue + Color.FgWhite, caller, Color.Reset,
-					message,
-					Color.FgGreen, object, Color.Reset
-				);
-				console.groupEnd();
-			} else if (type === 'bigint' || type === 'number' || type === 'string') {
-				console.group(
-					Color.BgWhite + Color.FgBlack, className, Color.Reset,
-					Color.BgBlue + Color.FgWhite, caller, Color.Reset,
-					message,
-					Color.FgGreen, object, Color.Reset
-				);
-				console.groupEnd();
-			} else {
-				console.group(
-					Color.BgWhite + Color.FgBlack, className, Color.Reset,
-					Color.BgBlue + Color.FgWhite, caller, Color.Reset,
-					message
-				);
-				console.log(object, Color.Reset);
-				console.groupEnd();
-			}
-		} else if (object === false) {
+	if (!Constants.IN_DEBUG_MODE) {
+		return;
+	}
+
+	if (!(caller.includes('(') || caller.includes(')'))) {
+		caller = caller + '()';
+	}
+	if (caller.includes('^')) {
+		caller = caller.replace('^', '');
+		caller = caller + '^';
+	}
+	if (object) {
+		const type = typeof (object);
+		// console.log('type:', type);
+		if (type === 'boolean') {
+			console.group(
+				Color.BgWhite + Color.FgBlack, className, Color.Reset,
+				Color.BgBlue + Color.FgWhite, caller, Color.Reset,
+				message,
+				Color.FgGreen, object, Color.Reset
+			);
+			console.groupEnd();
+		} else if (type === 'bigint' || type === 'number' || type === 'string') {
 			console.group(
 				Color.BgWhite + Color.FgBlack, className, Color.Reset,
 				Color.BgBlue + Color.FgWhite, caller, Color.Reset,
@@ -58,9 +44,25 @@ const log = function (
 			console.group(
 				Color.BgWhite + Color.FgBlack, className, Color.Reset,
 				Color.BgBlue + Color.FgWhite, caller, Color.Reset,
-				message, Color.Reset
+				message
 			);
+			console.log(object, Color.Reset);
 			console.groupEnd();
 		}
+	} else if (object === false) {
+		console.group(
+			Color.BgWhite + Color.FgBlack, className, Color.Reset,
+			Color.BgBlue + Color.FgWhite, caller, Color.Reset,
+			message,
+			Color.FgGreen, object, Color.Reset
+		);
+		console.groupEnd();
+	} else {
+		console.group(
+			Color.BgWhite + Color.FgBlack, className, Color.Reset,
+			Color.BgBlue + Color.FgWhite, caller, Color.Reset,
+			message, Color.Reset
+		);
+		console.groupEnd();
 	}
 };
